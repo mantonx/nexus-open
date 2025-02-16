81054d1737c66079158396f39fe148f340f588fa
Matt <matt@projectguacamole.com>
2025-02-16 15:28:47 -0500

    USB works now

---
+++ go.mod
@@ -0,0 +1,8 @@
+module my-go-cli
+
+go 1.18
+
+require (
+	github.com/google/gousb v1.1.3 // indirect
+	golang.org/x/image v0.24.0 // indirect
+)
---
+++ pkg/example/example.go
@@ -0,0 +1,8 @@
+package example
+
+import "fmt"
+
+// RunExample performs a sample operation and prints a message to the console.
+func RunExample() {
+    fmt.Println("This is an example function from the example package.")
+}
